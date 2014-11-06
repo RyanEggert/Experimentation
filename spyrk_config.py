5106f3689d790d31a560ece471f7fa1878a1fdd1
Ryan Eggert <Ryan.Eggert@students.olin.edu>
2014-11-06 15:04:07 -0500

    Deletion
    Dang.

--- spyrk_config.py
+++
@@ -1,8 +0,0 @@
-# spyrk_config.py
-
-
-authentication = {
-                'username':'ryaneggertwi@gmail.com',
-                'password': 'J1SAlE2coRTN@NlBDICY',
-                'accesstoken': 'b634197dec502543ceb499953f7094240f03d234'
-                }
